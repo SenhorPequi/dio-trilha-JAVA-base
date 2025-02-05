6456fa73fe33c1ed343772d9c7870f3a727487f2
francisco <francisconetoalvesgomes@gmail.com>
2025-02-05 17:32:23 -0300

    Projeto1

--- Tipos-variaveis/tipo-de-variaveis/scr/ContaBancaria.java
+++ Tipos-variaveis/tipo-de-variaveis/scr/ContaBancaria.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class ContaBancaria {
 
      public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int numero, Agencia,
+        String numero, Agencia, NomedoCliente, Saldo;
+
+        System.out.println("Informe o numero: ");
+        numero = teclado.nextLine();
+
+        System.out.println("Informe sua agencia bancaria: ");
+        Agencia = teclado.nextLine();
+
+        System.out.println("Informe o seu nome: ");
+        NomedoCliente = teclado.nextLine();
+
+        System.out.println("Saldo bancario: ");
+        Saldo = teclado.nextLine();
+
+        System.out.println("\nOlá " + NomedoCliente + ", obrigado por criar uma conta em nosso banco, "
+                + "sua agência é " + Agencia + ", conta " + numero + " e seu saldo "
+                + Saldo + " já está disponível para saque.");
+
+
+        teclado.close();
 
 
      }
