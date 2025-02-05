ab5160c96a3b9180c1ffdd50003c00f32392d358
francisco <francisconetoalvesgomes@gmail.com>
2025-02-05 17:26:57 -0300

    Primeiro Desafio

---
+++ Tipos-variaveis/tipo-de-variaveis/scr/ContaBancaria.java
@@ -0,0 +1,11 @@
+public class ContaBancaria {
+
+     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero, Agencia,
+
+
+     }
+
+    
+}
